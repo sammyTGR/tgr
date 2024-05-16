c7c7c34af5441a15f47b6f826bb37e27cceab5f9
kozyguns <149282419+kozyguns@users.noreply.github.com>
2024-05-16 14:59:11 +0000

    fixed config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,11 +7,6 @@ module.exports = {
     './app/**/*.{ts,tsx}',
     './src/**/*.{ts,tsx}',
   ],
-  // safelist: [
-  //   'bg-red-500',
-  //   'text-3xl',
-  //   'lg:text-4xl',
-  // ],
   prefix: "",
   theme: {
     container: {
@@ -71,15 +66,7 @@ module.exports = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
-        "meteor-effect": "meteor 5s linear infinite",
-      },
-      animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
-        "meteor-effect": "meteor 5s linear infinite",
-      },
-      keyframes: {
-        meteor: {
+        "meteor": {
           "0%": { transform: "rotate(215deg) translateX(0)", opacity: 1 },
           "70%": { opacity: 1 },
           "100%": {
@@ -87,6 +74,11 @@ module.exports = {
             opacity: 0,
           },
         },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+        "meteor": "meteor 5s linear infinite",
       },
     },
   },
