776a11d0998853f232e7d094b5a43f43b81ec951
Sam Lee <samlee@range.local>
2024-05-22 05:15:52 -0700

    stashing

--- src/app/auditreview/page.tsx
+++ src/app/auditreview/page.tsx
@@ -3,18 +3,16 @@ import supabase from "../../../supabase/lib/supabaseClient";
 import { AuditData, columns } from "./columns";
 import { DataTable } from "@/components/ui/data-table";
 import { TextGenerateEffect } from "@/components/ui/text-generate-effect";
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
-const words = 'Audits';
+const words = "Audits";
 
 async function fetchAuditData(): Promise<AuditData[]> {
-  const { data, error } = await supabase
-    .from("Auditsinput")
-    .select("*");
-    // .order('audit_date', { ascending: true }); // This ensures sorting is handled by Supabase
+  const { data, error } = await supabase.from("Auditsinput").select("*");
+  // .order('audit_date', { ascending: true }); // This ensures sorting is handled by Supabase
 
   if (error) {
-    console.error('Error fetching initial data:', error.message);
+    console.error("Error fetching initial data:", error.message);
     throw new Error(error.message);
   }
   return data as AuditData[];
@@ -33,33 +31,36 @@ export default function AuditReview() {
         setData(fetchedData);
         setLoading(false);
       } catch (error) {
-        console.error('Failed to fetch data:', error);
+        console.error("Failed to fetch data:", error);
         setLoading(false);
       }
     };
-  
+
     fetchData();
-  
+
     const subscription = supabase
-    .channel('custom-all-audits-channel')
-    .on('postgres_changes', { event: '*', schema: 'public', table: 'Auditsinput' }, payload => {
-      console.log('Real-time change received:', payload);
-      if (payload.new) {
-        setData(currentData => [
-          ...currentData, 
-          payload.new as AuditData // Assert that payload.new is of type AuditData
-        ]);
-      } else {
-        fetchData(); // Refetch for other types of updates
-      }
-    })
-    .subscribe();
+      .channel("custom-all-audits-channel")
+      .on(
+        "postgres_changes",
+        { event: "*", schema: "public", table: "Auditsinput" },
+        (payload) => {
+          console.log("Real-time change received:", payload);
+          if (payload.new) {
+            setData((currentData) => [
+              ...currentData,
+              payload.new as AuditData, // Assert that payload.new is of type AuditData
+            ]);
+          } else {
+            fetchData(); // Refetch for other types of updates
+          }
+        }
+      )
+      .subscribe();
     // Cleanup function for the useEffect
     return () => {
       supabase.removeChannel(subscription);
     };
   }, []);
-  
 
   return (
     <div>
@@ -67,14 +68,20 @@ export default function AuditReview() {
         <div className="hidden h-full flex flex-col space-y-8 p-8 md:flex">
           <div className="flex items-center justify-between space-y-2">
             <div>
-              <h2 className="text-2xl font-bold"><TextGenerateEffect words={words} /></h2>
+              <h2 className="text-2xl font-bold">
+                <TextGenerateEffect words={words} />
+              </h2>
             </div>
           </div>
           <div className="flex items-center justify-between space-y-2">
             <div className="space-y-4">
               <div className="rounded-md border">
                 <div className="relative w-full overflow-auto">
-                  {loading ? <p>Loading...</p> : <DataTable columns={columns} data={data} />}
+                  {loading ? (
+                    <p>Loading...</p>
+                  ) : (
+                    <DataTable columns={columns} data={data} />
+                  )}
                 </div>
               </div>
             </div>
