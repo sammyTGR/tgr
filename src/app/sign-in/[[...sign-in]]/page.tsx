617d6c98b9b369c69fd84b6a074350cf350c485e
kozyguns <149282419+kozyguns@users.noreply.github.com>
2024-05-31 22:47:09 -0700

    updates

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -118,7 +118,7 @@ export default function SignIn() {
             </Button>
           </form>
           <div className="mt-4 text-center text-sm">
-            Don't have an account?{" "}
+            Don&apos;t have an account?{" "}
             <Link href="/sign-up" className="underline">
               Sign up
             </Link>
