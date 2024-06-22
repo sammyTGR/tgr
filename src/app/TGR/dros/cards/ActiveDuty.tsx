dc633616eda07c1f6d66b6f810761e4c34dccf89
Sam Lee <samlee@range.local>
2024-06-22 12:05:05 -0700

    updated fed doc worksheet

--- src/app/TGR/dros/cards/ActiveDuty.tsx
+++ src/app/TGR/dros/cards/ActiveDuty.tsx
@@ -1,63 +1,85 @@
-import * as React from "react"
+import * as React from "react";
 import { useRouter } from "next/navigation";
-  import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle,
-  } from "@/components/ui/card"
-import { HoveredLink, Menu, MenuItem, ProductItem } from "@/components/ui/navbar-menu";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  HoveredLink,
+  Menu,
+  MenuItem,
+  ProductItem,
+} from "@/components/ui/navbar-menu";
 
-
-  function ActiveDuty ({ className }: { className?: string }) {
+function ActiveDuty({ className }: { className?: string }) {
   const router = useRouter();
 
-    return (
-        <div className="flex flex-col items-center justify-center w-full" >
-            <Card className="flex flex-col w-full">
-                <CardHeader>
-                <CardTitle>Active Duty Out Of State Info</CardTitle>
-                <CardDescription>
-                <span className="text-orange-500">Before Moving Forward - </span><br/>
-                    <span className="text-orange-500">PRINT</span> THE Federal Doc Worksheet In The Link Below<br/>
-                    <hr className="my-4" />
-                      <br/>If The Transaction Is For A <span className="text-blue-500">Blue Label</span> Firearm,<br/>
-                      Don&apos;t Forget To Fill Out The <span className="text-blue-500">Blue Label</span> Form & Include It In The Pending Packet
-                    </CardDescription>
-                </CardHeader>
-                <CardContent>
-                  <div className="max-w-[425px]">
-                    <h2>Required Documents:</h2>
-                    <ul className="list-disc pl-4">
-                      <li>PCS Orders Containing Effective Date & Order Number</li>
-                      <li>Proof Of CA Residence (Anything Listed In &quot;Address Correction Docs&quot;)</li>
-                      <li>
-                        Photo Copy Out Of State DL / ID As Normal
-                        <ul className="list-none pl-4">
-                          <li>
-                            Write Down DOD ID#, Rank & Branch On 
-                            <span className="text-orange-500"> Federal Doc Worksheet 
-                              <br className="lg:hidden" />
-                              <span className="lg:inline"> (Printable Link Below)</span>
-                            </span>
-                          </li>
-                        </ul>
-                      </li>
-                    </ul>
-                    <hr className="my-4" />
-                    <p>FSC Exemption Code</p>
-                    <p>(X21 - Military - Active Duty)</p>
-                    <hr className="my-4" />
-                    <p>Military Are <span className="text-red-500">NOT</span> Roster | 30 Day Exempt</p>
-                  </div>
-                </CardContent>
-                <CardFooter className="flex justify-between">
-                <HoveredLink href="https://utfs.io/f/dee78aeb-f225-49a3-b669-f28f475cf6ff-n171pw.pdf"><span className="text-orange-500">Federal Doc Worksheet</span></HoveredLink>
-                </CardFooter>
-            </Card>
-        </div>
-    )
-  }
-  export default ActiveDuty;
+  return (
+    <div className="flex flex-col items-center justify-center w-full">
+      <Card className="flex flex-col w-full">
+        <CardHeader>
+          <CardTitle>Active Duty Out Of State Info</CardTitle>
+          <CardDescription>
+            <span className="text-orange-500">Before Moving Forward - </span>
+            <br />
+            <span className="text-orange-500">PRINT</span> THE Federal Doc
+            Worksheet In The Link Below
+            <br />
+            <hr className="my-4" />
+            <br />
+            If The Transaction Is For A{" "}
+            <span className="text-blue-500">Blue Label</span> Firearm,
+            <br />
+            Don&apos;t Forget To Fill Out The{" "}
+            <span className="text-blue-500">Blue Label</span> Form & Include It
+            In The Pending Packet
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div className="max-w-[425px]">
+            <h2>Required Documents:</h2>
+            <ul className="list-disc pl-4">
+              <li>PCS Orders Containing Effective Date & Order Number</li>
+              <li>
+                Proof Of CA Residence (Anything Listed In &quot;Address
+                Correction Docs&quot;)
+              </li>
+              <li>
+                Photo Copy Out Of State DL / ID As Normal
+                <ul className="list-none pl-4">
+                  <li>
+                    Write Down DOD ID#, Rank & Branch On
+                    <span className="text-orange-500">
+                      {" "}
+                      Federal Doc Worksheet
+                      <br className="lg:hidden" />
+                      <span className="lg:inline"> (Printable Link Below)</span>
+                    </span>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+            <hr className="my-4" />
+            <p>FSC Exemption Code</p>
+            <p>(X21 - Military - Active Duty)</p>
+            <hr className="my-4" />
+            <p>
+              Military Are <span className="text-red-500">NOT</span> Roster | 30
+              Day Exempt
+            </p>
+          </div>
+        </CardContent>
+        <CardFooter className="flex justify-between">
+          <HoveredLink href="https://utfs.io/f/9663420d-2204-4991-b7c9-a1e6239e6a3b-n171pw.pdf">
+            <span className="text-orange-500">Federal Doc Worksheet</span>
+          </HoveredLink>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+}
+export default ActiveDuty;
--- src/app/TGR/dros/cards/AmmoPurchase.tsx
+++ src/app/TGR/dros/cards/AmmoPurchase.tsx
@@ -1,21 +1,21 @@
-import * as React from "react"
+import * as React from "react";
 import { useRouter } from "next/navigation";
-import { Tabs, 
-  TabsContent, 
-  TabsList, 
-  TabsTrigger 
-} from "@/components/ui/tabs"
-  import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle,
-  } from "@/components/ui/card"
-import { HoveredLink, Menu, MenuItem, ProductItem } from "@/components/ui/navbar-menu";
-import styled from 'styled-components';
-
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  HoveredLink,
+  Menu,
+  MenuItem,
+  ProductItem,
+} from "@/components/ui/navbar-menu";
+import styled from "styled-components";
 
 const StyledTabsList = styled(TabsList)`
   display: flex;
@@ -28,104 +28,144 @@ const StyledTabsList = styled(TabsList)`
   }
 `;
 
-  
-  function AmmoPurchase ({ className }: { className?: string }) {
+function AmmoPurchase({ className }: { className?: string }) {
   const router = useRouter();
 
-    return (
-        <div className="flex flex-col items-center justify-center w-[515px]" >  
-        <Card className="flex flex-col w-full">
-            <CardHeader>
-            <CardTitle>Ammo Sales Guide</CardTitle>
-            <CardDescription>
-                Customer Must Have Purchased A Firearm Within The Last 5 Years At Current Physical Address<br/>
-                Otherwise, They Must Update Their Address With <span className="text-amber-500">CFARS</span> (Link Below)
-                </CardDescription>
-            </CardHeader>
-            <CardContent>
-            <Tabs defaultValue="" className="w-full">
-          <TabsList className=" grid grid-cols-2 mb-4">
-            <TabsTrigger value="address">Address Correction</TabsTrigger>
-            <TabsTrigger value="federal">Federal Limits Apply</TabsTrigger>
-            <TabsTrigger value="oos">Out Of State Military (ACTIVE DUTY WITH PCS)</TabsTrigger>
-          </TabsList>
-          <TabsContent value="address">
-          <div className="flex flex-col w-full">
-                <h2>If The Address On CA DL | ID Is <span className="text-red-500">NOT</span> Current, One Of The Following Docs Can Be Used For Address Correction:</h2>
-                    <ul className="list-disc pl-4">
-                        <li>Valid CA Hunting License</li>
-                        <li>Valid CA Fishing License</li>
-                        <li>Utility Bill Dated Within The Last 90 Days</li>
-                        <li>Valid CA DMV Registration</li>
-                        <li>Valid CA CCW Permit</li>
-                        <li>Along With Any Other Doc Listed In &quot;Address Correction Docs&quot;</li>
-                    </ul>
-                </div>
-                </TabsContent>
-          <TabsContent value="federal">
-          <div className="max-w-full">
-            <h2 >Customer Must Have One Of The Following Docs:</h2>
-            <ul className="list-none pl-4">
-            <li>Valid Passport</li>
-            <li>Certified Birth Certificate</li>
-            </ul>
-            <p>These Will Be <span className="text-amber-500">IN ADDITION TO</span> A Valid CA ID | DL</p>
-            </div>
-            <hr className="my-4" />
+  return (
+    <div className="flex flex-col items-center justify-center w-[515px]">
+      <Card className="flex flex-col w-full">
+        <CardHeader>
+          <CardTitle>Ammo Sales Guide</CardTitle>
+          <CardDescription>
+            Customer Must Have Purchased A Firearm Within The Last 5 Years At
+            Current Physical Address
+            <br />
+            Otherwise, They Must Update Their Address With{" "}
+            <span className="text-amber-500">CFARS</span> (Link Below)
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Tabs defaultValue="" className="w-full">
+            <TabsList className=" grid grid-cols-2 mb-4">
+              <TabsTrigger value="address">Address Correction</TabsTrigger>
+              <TabsTrigger value="federal">Federal Limits Apply</TabsTrigger>
+              <TabsTrigger value="oos">
+                Out Of State Military (ACTIVE DUTY WITH PCS)
+              </TabsTrigger>
+            </TabsList>
+            <TabsContent value="address">
+              <div className="flex flex-col w-full">
+                <h2>
+                  If The Address On CA DL | ID Is{" "}
+                  <span className="text-red-500">NOT</span> Current, One Of The
+                  Following Docs Can Be Used For Address Correction:
+                </h2>
+                <ul className="list-disc pl-4">
+                  <li>Valid CA Hunting License</li>
+                  <li>Valid CA Fishing License</li>
+                  <li>Utility Bill Dated Within The Last 90 Days</li>
+                  <li>Valid CA DMV Registration</li>
+                  <li>Valid CA CCW Permit</li>
+                  <li>
+                    Along With Any Other Doc Listed In &quot;Address Correction
+                    Docs&quot;
+                  </li>
+                </ul>
+              </div>
             </TabsContent>
-          <TabsContent value="oos">
-          <div className="flex flex-col w-full">
-            <h2>Customer Must Have The Following:</h2>
-                    <ul className="list-disc pl-4">
-                      <li>PCS Orders Containing Effective Date & Order Number</li>
-                      <li>Proof Of CA Residence (Anything Listed In &quot;Address Correction Docs&quot;)</li>
-                      </ul>
-                      <hr className="my-4" />
-                      <p>Photo Copy Out Of State DL | ID As Normal</p>
-                      <p>Since We Cannot Photocopy Their Military ID - Write Down DOD ID#, Rank & Branch On <span className="text-orange-500">Federal Worksheet (Link Below)</span> </p>
-                      <p><span className="text-red-500">DO NOT USE</span> Any Other Address Other Than What&apos;s Listed On Their ID | DL</p>
-                      <hr className="my-4" />
-                      <p>If The Customer States They Went To Another Shop & They Utilized A Different Address,
-                      Direct The Customer To The CFARS Website To Update Their Address (Link Found Below)
-                      <hr className="my-4" />
-                      We Must <span className="text-red-500">ALWAYS & ONLY</span> Process Their DROS With The Address On Their ID | DL</p>
-            </div>
+            <TabsContent value="federal">
+              <div className="max-w-full">
+                <h2>Customer Must Have One Of The Following Docs:</h2>
+                <ul className="list-none pl-4">
+                  <li>Valid Passport</li>
+                  <li>Certified Birth Certificate</li>
+                </ul>
+                <p>
+                  These Will Be{" "}
+                  <span className="text-amber-500">IN ADDITION TO</span> A Valid
+                  CA ID | DL
+                </p>
+              </div>
+              <hr className="my-4" />
+            </TabsContent>
+            <TabsContent value="oos">
+              <div className="flex flex-col w-full">
+                <h2>Customer Must Have The Following:</h2>
+                <ul className="list-disc pl-4">
+                  <li>PCS Orders Containing Effective Date & Order Number</li>
+                  <li>
+                    Proof Of CA Residence (Anything Listed In &quot;Address
+                    Correction Docs&quot;)
+                  </li>
+                </ul>
+                <hr className="my-4" />
+                <p>Photo Copy Out Of State DL | ID As Normal</p>
+                <p>
+                  Since We Cannot Photocopy Their Military ID - Write Down DOD
+                  ID#, Rank & Branch On{" "}
+                  <span className="text-orange-500">
+                    Federal Worksheet (Link Below)
+                  </span>{" "}
+                </p>
+                <p>
+                  <span className="text-red-500">DO NOT USE</span> Any Other
+                  Address Other Than What&apos;s Listed On Their ID | DL
+                </p>
+                <hr className="my-4" />
+                <p>
+                  If The Customer States They Went To Another Shop & They
+                  Utilized A Different Address, Direct The Customer To The CFARS
+                  Website To Update Their Address (Link Found Below)
+                  <hr className="my-4" />
+                  We Must <span className="text-red-500">
+                    ALWAYS & ONLY
+                  </span>{" "}
+                  Process Their DROS With The Address On Their ID | DL
+                </p>
+              </div>
             </TabsContent>
             <TabsContent value="withletter">
-            <div className="flex flex-col w-full">
-                <h2><span className="text-amber-500">Start By Selecting &quot;Peace Officer Non-Roster Handgun Sale (Letter Required)&quot;</span></h2>
+              <div className="flex flex-col w-full">
+                <h2>
+                  <span className="text-amber-500">
+                    Start By Selecting &quot;Peace Officer Non-Roster Handgun
+                    Sale (Letter Required)&quot;
+                  </span>
+                </h2>
                 <hr className="my-4" />
                 <ul className="list-none pl-4">
-                    <p>FSC Exemption Code</p>
-                        <ul className="list-none pl-4">
-                        <li>(X31 - PEACE OFFICER - CALIFORNIA - ACTIVE)</li>
-                        </ul>
-                        <p>30-Day Restriction Exemption</p>
-                        <ul className="list-none pl-4">
-                            <li>(PEACE OFFICER - ACTIVE - LETTER REQUIRED)</li>
-                        </ul>
-                        <p>Waiting Period Exemption | 10 Day Wait</p>
-                        <ul className="list-none pl-4">
-                            <li>(PEACE OFFICER (LETTER REQUIRED))</li>
-                        </ul>
-                    <p>Non-Roster Exemption</p>
-                        <ul className="list-none pl-4">
-                            <li>(AGENCY THEY ARE EMPLOYED WITH)</li>
-                        </ul>
-                        </ul>
-                </div>
-                <hr className="my-4" />
-                </TabsContent>
-                </Tabs>
-            </CardContent>
-            <CardFooter className="flex justify-between">
-            <a href="https://utfs.io/f/dee78aeb-f225-49a3-b669-f28f475cf6ff-n171pw.pdf"><span className="text-orange-500">Federal Doc Worksheet</span></a>
-            <a href="https://cfars.doj.ca.gov/login"><span className="text-orange-500">CFARS</span></a>  
-
-            </CardFooter>
-            </Card>
-            
-        </div>
-    )
-  }
-  export default AmmoPurchase;
+                  <p>FSC Exemption Code</p>
+                  <ul className="list-none pl-4">
+                    <li>(X31 - PEACE OFFICER - CALIFORNIA - ACTIVE)</li>
+                  </ul>
+                  <p>30-Day Restriction Exemption</p>
+                  <ul className="list-none pl-4">
+                    <li>(PEACE OFFICER - ACTIVE - LETTER REQUIRED)</li>
+                  </ul>
+                  <p>Waiting Period Exemption | 10 Day Wait</p>
+                  <ul className="list-none pl-4">
+                    <li>(PEACE OFFICER (LETTER REQUIRED))</li>
+                  </ul>
+                  <p>Non-Roster Exemption</p>
+                  <ul className="list-none pl-4">
+                    <li>(AGENCY THEY ARE EMPLOYED WITH)</li>
+                  </ul>
+                </ul>
+              </div>
+              <hr className="my-4" />
+            </TabsContent>
+          </Tabs>
+        </CardContent>
+        <CardFooter className="flex justify-between">
+          <a href="https://utfs.io/f/9663420d-2204-4991-b7c9-a1e6239e6a3b-n171pw.pdf">
+            <span className="text-orange-500">Federal Doc Worksheet</span>
+          </a>
+          <a href="https://cfars.doj.ca.gov/login">
+            <span className="text-orange-500">CFARS</span>
+          </a>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+}
+export default AmmoPurchase;
