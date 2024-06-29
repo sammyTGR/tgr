61fa062464510327537e733b3557787094410eea
Sam Lee <samlee@range.local>
2024-06-29 09:43:03 -0700

    updated chat client

--- src/components/NotificationsProvider.tsx
+++ src/components/NotificationsProvider.tsx
@@ -1,8 +1,9 @@
 "use client";
 
+import { ReactNode } from "react";
 import useRealtimeNotifications from "@/utils/useRealtimeNotifications";
 
-const NotificationsProvider = ({ children }: { children: React.ReactNode }) => {
+const NotificationsProvider = ({ children }: { children: ReactNode }) => {
   useRealtimeNotifications(); // Activate the real-time notifications
 
   return <>{children}</>; // Render children
