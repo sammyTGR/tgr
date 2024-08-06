9e622e6b601a8bbc59125d362805d983eb8ba760
kozyguns <149282419+kozyguns@users.noreply.github.com>
2024-08-06 04:43:03 -0700

    hidden clock

--- src/app/TGR/crew/profile/[employeeId]/page.tsx
+++ src/app/TGR/crew/profile/[employeeId]/page.tsx
@@ -603,7 +603,7 @@ const EmployeeProfilePage = () => {
           <Tabs defaultValue="schedules" className="w-full">
             <TabsList className="border-b border-gray-200 dark:border-gray-700">
               <TabsTrigger value="schedules">Scheduling</TabsTrigger>
-              <TabsTrigger value="clock">Timesheet</TabsTrigger>
+              {/* <TabsTrigger value="clock">Timesheet</TabsTrigger> */}
               <TabsTrigger value="performance">Sales & Audits</TabsTrigger>
               <TabsTrigger value="forms">Forms</TabsTrigger>
               <TabsTrigger value="reviews">Reviews</TabsTrigger>
