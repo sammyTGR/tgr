febb5121dfb5f837b33325568655efd407df0eac
Sam Lee <kozyguns@gmail.com>
2024-08-08 12:06:19 -0700

    updated change password page

--- src/app/change-password/page.tsx
+++ src/app/change-password/page.tsx
@@ -1,69 +1,48 @@
 "use client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
 import { supabase } from "@/utils/supabase/client";
 import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { toast } from "sonner";
-import { useForm, Controller } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { z } from "zod";
-import { useRouter } from "next/navigation";
-
-// Define the validation schema using Zod
-const schema = z
-  .object({
-    password: z
-      .string()
-      .min(6, { message: "Password must be at least 6 characters" }),
-    confirmPassword: z
-      .string()
-      .min(6, { message: "Confirm Password must be at least 6 characters" }),
-  })
-  .refine((data) => data.password === data.confirmPassword, {
-    message: "Passwords do not match",
-    path: ["confirmPassword"],
-  });
-
-type FormData = z.infer<typeof schema>;
 
 export default function ChangePassword() {
-  const {
-    handleSubmit,
-    control,
-    formState: { errors },
-  } = useForm<FormData>({
-    resolver: zodResolver(schema),
-  });
-  const router = useRouter();
+  const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(false);
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const accessToken = searchParams?.get("access_token");
 
-  const onSubmit = async (data: FormData) => {
-    const { password } = data;
+  useEffect(() => {
+    if (!accessToken) {
+      toast.error("Invalid or missing token!");
+      router.push("/sign-in");
+    } else {
+      supabase.auth.setSession({
+        access_token: accessToken,
+        refresh_token: "",
+      });
+    }
+  }, [accessToken, router]);
+
+  const handlePasswordChange = async () => {
+    setLoading(true);
 
     try {
-      setLoading(true);
-      const { error } = await supabase.auth.updateUser({ password });
+      const { error } = await supabase.auth.updateUser({
+        password,
+      });
 
-      if (error) throw error;
-
-      toast.success("Password changed successfully");
-      router.push("/sign-in"); // Redirect to sign-in page after password change
-    } catch (error) {
-      if (error instanceof Error) {
-        console.error("Error changing password:", error.message);
+      if (error) {
         toast.error(error.message);
       } else {
-        console.error("Unexpected error changing password:", error);
-        toast.error("Unexpected error occurred.");
+        toast.success("Password updated successfully");
+        router.push("/sign-in");
       }
+    } catch (error) {
+      toast.error("An error occurred while updating the password");
     } finally {
       setLoading(false);
     }
@@ -74,56 +53,28 @@ export default function ChangePassword() {
       <Card className="mx-auto max-w-md">
         <CardHeader>
           <CardTitle className="text-xl">Change Password</CardTitle>
-          <CardDescription>Enter your new password</CardDescription>
         </CardHeader>
         <CardContent>
-          <form onSubmit={handleSubmit(onSubmit)} className="grid gap-4 mt-4">
+          <div className="grid gap-4">
             <div className="grid gap-2">
               <Label htmlFor="password">New Password</Label>
-              <Controller
-                name="password"
-                control={control}
-                render={({ field }) => (
-                  <Input
-                    {...field}
-                    id="password"
-                    type="password"
-                    placeholder="Enter your new password"
-                    required
-                  />
-                )}
+              <Input
+                id="password"
+                type="password"
+                placeholder="Enter your new password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
               />
-              {errors.password && (
-                <span className="text-red-500 text-xs">
-                  {errors.password.message}
-                </span>
-              )}
             </div>
-            <div className="grid gap-2">
-              <Label htmlFor="confirmPassword">Confirm New Password</Label>
-              <Controller
-                name="confirmPassword"
-                control={control}
-                render={({ field }) => (
-                  <Input
-                    {...field}
-                    id="confirmPassword"
-                    type="password"
-                    placeholder="Confirm your new password"
-                    required
-                  />
-                )}
-              />
-              {errors.confirmPassword && (
-                <span className="text-red-500 text-xs">
-                  {errors.confirmPassword.message}
-                </span>
-              )}
-            </div>
-            <Button type="submit" className="w-full" disabled={loading}>
-              {loading ? "Changing..." : "Change Password"}
+            <Button
+              onClick={handlePasswordChange}
+              className="w-full"
+              disabled={loading}
+            >
+              {loading ? "Updating..." : "Update Password"}
             </Button>
-          </form>
+          </div>
         </CardContent>
       </Card>
     </div>
