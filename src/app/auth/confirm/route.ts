3065b1aa46ad9032cfc92e52722dc6a6cc263e56
Sam Lee <kozyguns@gmail.com>
2024-08-08 13:34:14 -0700

    updated route

--- src/app/auth/confirm/route.ts
+++ src/app/auth/confirm/route.ts
@@ -4,30 +4,29 @@ import { NextRequest, NextResponse } from 'next/server';
 import { createClient } from '@/utils/supabase/server';
 
 export async function GET(request: NextRequest) {
-  const { searchParams, origin } = new URL(request.url);
+  const { searchParams } = new URL(request.url);
   const token_hash = searchParams.get('token_hash');
   const type = searchParams.get('type') as EmailOtpType | null;
   const next = searchParams.get('next') ?? '/';
-
-  console.log('token_hash:', token_hash);
-  console.log('type:', type);
-  console.log('next:', next);
+  const redirectTo = request.nextUrl.clone();
+  redirectTo.pathname = next;
 
   if (token_hash && type) {
-    const cookieStore = cookies();
     const supabase = createClient();
+
     const { error } = await supabase.auth.verifyOtp({
       type,
       token_hash,
     });
 
     if (!error) {
-      return NextResponse.redirect(`${origin}${next}`);
+      return NextResponse.redirect(redirectTo);
     } else {
       console.error('verifyOtp error:', error.message);
     }
   }
 
   // return the user to an error page with instructions
-  return NextResponse.redirect(`${origin}/auth/auth-code-error`);
+  redirectTo.pathname = '/auth/auth-code-error';
+  return NextResponse.redirect(redirectTo);
 }
