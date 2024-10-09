888684b7d1af631316cbd28f656ba561194f5d43
Sam Lee <kozyguns@gmail.com>
2024-10-09 11:45:13 -0700

    updated dashboard to utilize new todos

--- src/app/admin/todo/actions/route.ts
+++
@@ -1,8 +0,0 @@
-"use server";
-
-import { editTodo as editTodoAction } from "../actions";
-import type { Todo } from "@/lib/interface";
-
-export async function editTodo(todo: Todo) {
-  return await editTodoAction(todo);
-}
