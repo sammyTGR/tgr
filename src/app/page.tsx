d30bd076085829f0a477e83db9e62bf1aa42eef6
Sam Lee <kozyguns@gmail.com>
2024-10-10 12:52:44 -0700

    added flagsmith to todo and chat and reverted middleware and pagetsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,10 @@
 "use client";
-import { Suspense, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { supabase } from "@/utils/supabase/client";
 import LandingPagePublic from "@/components/LandingPagePublic";
 import LandingPageCustomer from "@/components/LandingPageCustomer";
 import { ProgressBar } from "@/components/ProgressBar";
-import { FlagValues } from "@vercel/flags/react";
-import { encrypt } from "@vercel/flags";
-import { useFlagsmith } from "flagsmith/react";
 
 export default function Home() {
   const [loading, setLoading] = useState(true);
@@ -16,97 +13,123 @@ export default function Home() {
   const [progress, setProgress] = useState(0);
   const [role, setRole] = useState<string | null>(null);
   const router = useRouter();
-  const flagsmith = useFlagsmith();
-
-  const flagValues = { is_chat_enabled: false, is_todo_enabled: false }; // Define your feature flags here
 
   useEffect(() => {
     const fetchRoleAndRedirect = async () => {
       setProgress(10); // Initial progress
 
-      const { data: userData, error: userError } =
-        await supabase.auth.getUser();
+      const {
+        data: { session },
+        error: sessionError,
+      } = await supabase.auth.getSession();
 
-      if (userError || !userData.user) {
-        console.error(
-          "Error fetching user or no active user found:",
-          userError?.message || "User data missing!"
-        );
+      if (sessionError || !session) {
+        // console.error(
+        //   "Error fetching session or no active session found:",
+        //   sessionError?.message || "Auth session missing!"
+        // );
         setLoading(false);
         setNoSession(true);
         setProgress(100); // Final progress
-        return; // No active user found
+        return; // No active session found
       }
 
-      const user = userData.user;
       setRoleValidating(true); // Start role validation
       setProgress(30); // Update progress
 
+      const { data: userData, error: userError } =
+        await supabase.auth.getUser();
+      if (userError) {
+        //console.("Error fetching user:", userError.message);
+        setLoading(false);
+        setRoleValidating(false); // End role validation
+        setNoSession(true);
+        setProgress(100); // Final progress
+        return; // Fetching the user failed
+      }
+
+      const user = userData.user;
+      setProgress(50); // Update progress
+
       // Check the employees table
-      const { data: employeeData, error: employeeError } = await supabase
+      const { data: roleData, error: roleError } = await supabase
         .from("employees")
         .select("role, employee_id")
-        .eq("user_uuid", user.id)
+        .eq("user_uuid", user?.id)
         .single();
 
-      if (employeeData) {
-        const { role, employee_id } = employeeData;
-        setRole(role);
-        setLoading(false);
-        setRoleValidating(false);
-        setProgress(100);
+      if (roleError || !roleData) {
+        // Check the customers table if not found in employees table
+        const { data: customerData, error: customerError } = await supabase
+          .from("customers")
+          .select("role")
+          .eq("email", user?.email)
+          .single();
 
-        if (["admin", "super admin", "dev"].includes(role)) {
+        if (customerError || !customerData) {
+          console.error(
+            "Error fetching role:",
+            roleError?.message || customerError?.message
+          );
+          setLoading(false);
+          setRoleValidating(false); // End role validation
+          setNoSession(true);
+          setProgress(100); // Final progress
+          return; // No role found
+        }
+
+        const role = customerData.role;
+        setRole(role);
+
+        if (role === "customer") {
+          setLoading(false);
+          setRoleValidating(false); // End role validation
+          setProgress(100); // Final progress
+          return; // Show the customer landing page
+        } else {
+          //console.("Invalid role for user:", user?.email);
+          setLoading(false);
+          setRoleValidating(false); // End role validation
+          setNoSession(true);
+          setProgress(100); // Final progress
+          return; // Invalid role
+        }
+      } else {
+        const { role, employee_id } = roleData;
+        setRole(role);
+        setLoading(false); // End loading before redirection
+        setRoleValidating(false); // End role validation
+        setProgress(100); // Final progress
+        if (role === "admin" || role === "super admin" || role === "dev") {
           router.push("/admin/reports/dashboard");
         } else {
           router.push(`/TGR/crew/profile/${employee_id}`);
         }
-        return;
-      }
-
-      // If not found in employees, check the customers table
-      const { data: customerData, error: customerError } = await supabase
-        .from("customers")
-        .select("role")
-        .eq("email", user.email)
-        .single();
-
-      if (customerData && customerData.role === "customer") {
-        setRole("customer");
-        setLoading(false);
-        setRoleValidating(false);
-        setProgress(100);
-        // Customer landing page will be shown
-      } else {
-        console.error(
-          "Error fetching role:",
-          employeeError?.message || customerError?.message || "Invalid role"
-        );
-        setLoading(false);
-        setRoleValidating(false);
-        setNoSession(true);
-        setProgress(100);
       }
     };
 
     fetchRoleAndRedirect();
   }, [router]);
 
-  return (
-    <>
-      {loading ? (
-        <div className="flex items-center justify-center h-full w-full">
-          <ProgressBar value={progress} showAnimation={true} />
-        </div>
-      ) : roleValidating ? (
-        <div>Validating Role...</div>
-      ) : noSession ? (
-        <LandingPagePublic />
-      ) : role === "customer" ? (
-        <LandingPageCustomer />
-      ) : null}
+  if (loading) {
+    return (
+      <div className="flex items-center justify-center h-full w-full">
+        <ProgressBar value={progress} showAnimation={true} />
+      </div>
+    ); // Show progress bar while loading
+  }
 
-      <FlagValues values={flagValues} />
-    </>
-  );
+  if (roleValidating) {
+    return <div>Validating Role...</div>; // Show role validation message
+  }
+
+  if (noSession) {
+    return <LandingPagePublic />; // Show the public landing page if no active session
+  }
+
+  if (role === "customer") {
+    return <LandingPageCustomer />;
+  }
+
+  return null; // No need to show anything else
 }
