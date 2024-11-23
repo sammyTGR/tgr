e510fbba98a17202d4a239b6fdafe94033bccd50
Sam Lee <kozyguns@gmail.com>
2024-11-23 10:43:59 -0800

    updated next config to remove headers

--- next.config.mjs
+++ next.config.mjs
@@ -13,28 +13,28 @@ const nextConfig = {
           },
         ],
     },
-    async headers() {
-      return [
-        {
-          source: "/api/:path*",
-          headers: [
-            { key: "Access-Control-Allow-Credentials", value: "true" },
-            { key: "Access-Control-Allow-Origin", value: "*" },
-            { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-            { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization, ApiKey, AppId" },
-          ]
-        }
-      ]
-    },
-    webpack: (config, { isServer }) => {
-      if (!isServer) {
-        config.resolve.fallback = {
-          ...config.resolve.fallback,
-          https: false,
-        };
-      }
-      return config;
-    }
+    // async headers() {
+    //   return [
+    //     {
+    //       source: "/api/:path*",
+    //       headers: [
+    //         { key: "Access-Control-Allow-Credentials", value: "true" },
+    //         { key: "Access-Control-Allow-Origin", value: "*" },
+    //         { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+    //         { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization, ApiKey, AppId" },
+    //       ]
+    //     }
+    //   ]
+    // },
+    // webpack: (config, { isServer }) => {
+    //   if (!isServer) {
+    //     config.resolve.fallback = {
+    //       ...config.resolve.fallback,
+    //       https: false,
+    //     };
+    //   }
+    //   return config;
+    // }
   
 };
 
