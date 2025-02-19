21546aaeaa690ad3c6d385c4aae327a8699fe574
Sam Lee <kozyguns@gmail.com>
2025-02-19 06:03:50 -0800

    updated dros redemption card

--- src/app/TGR/dros/cards/ConsignRedemp.tsx
+++ src/app/TGR/dros/cards/ConsignRedemp.tsx
@@ -1,44 +1,56 @@
-import * as React from "react"
+import * as React from "react";
 import { useRouter } from "next/navigation";
-  import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle,
-  } from "@/components/ui/card"
-import { HoveredLink, Menu, MenuItem, ProductItem } from "@/components/ui/navbar-menu";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  HoveredLink,
+  Menu,
+  MenuItem,
+  ProductItem,
+} from "@/components/ui/navbar-menu";
 
-  
-  function ConsignRedemp ({ className }: { className?: string }) {
+function ConsignRedemp({ className }: { className?: string }) {
   const router = useRouter();
 
-    return (
-        <div className="flex flex-col items-center justify-center w-[515px]" >
-            <Card className="flex flex-col w-full">
-                <CardHeader>
-                <CardTitle>Consignment Redemption DROS Guide</CardTitle>
-                <CardDescription>
-                    <h2><span className="text-red-500">NOTE:</span><br/>
-                    This Is Only Utilized In Cases Where We Are <span className="text-purple-500">Returning</span> The Consignment Seller&apos;s Firearm Back To Them</h2>
-                </CardDescription>
-                </CardHeader>
-                <CardContent>
-                <div className="max-w-full">
-                <p>FSC Exemption Code</p>
-                  <ul className="list-none pl-4">
-                  <li>(X03 - Return To Owner)</li>
-                  </ul>
-                  <hr className="my-4" />
-                <p>Utilize This FSC Exemption Code For <span className="text-purple-500">ALL</span> Consignment Redemptions</p>
-                </div>
-                </CardContent>
-                <CardFooter className="flex justify-between">
-                    
-                </CardFooter>
-            </Card>
-        </div>
-    )
-  }
-  export default ConsignRedemp;
+  return (
+    <div className="flex flex-col items-center justify-center w-[515px]">
+      <Card className="flex flex-col w-full">
+        <CardHeader>
+          <CardTitle>Consignment Redemption DROS Guide</CardTitle>
+          <CardDescription>
+            <span className="text-red-500">NOTE:</span> This Is Only Utilized In
+            Cases Where We Are{" "}
+            <span className="text-purple-500">Returning</span> The Consignment
+            Seller&apos;s Firearm Back To Them
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div className="max-w-full">
+            <li>Transaction Type</li>
+            <ul className="list-none pl-4">
+              <li>Pawn/Consignment Handgun Redemption</li>
+            </ul>
+            <li>FSC Exemption Code</li>
+            <ul className="list-none pl-4">
+              <li>(X03 - Return To Owner)</li>
+            </ul>
+            <hr className="my-4" />
+            <p>
+              Utilize This FSC Exemption Code For{" "}
+              <span className="text-purple-500">ALL</span> Consignment
+              Redemptions
+            </p>
+          </div>
+        </CardContent>
+        <CardFooter className="flex justify-between"></CardFooter>
+      </Card>
+    </div>
+  );
+}
+export default ConsignRedemp;
