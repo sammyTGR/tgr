95a950c14f053217c1cd10f124391778c36fffdf
Sam Lee <kozyguns@gmail.com>
2025-03-14 13:10:45 -0700

    removed loadingindicator from header

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -1,77 +1,30 @@
 "use client";
 import dynamic from "next/dynamic";
 import LoadingIndicator from "@/components/LoadingIndicator";
-import HeaderDev from "./HeaderDev";
-import HeaderUser from "./HeaderUser";
-import HeaderAdmin from "./HeaderAdmin";
-import HeaderSuperAdmin from "./HeaderSuperAdmin";
-import HeaderPublic from "./HeaderPublic";
-import HeaderCustomer from "./HeaderCustomer";
-import HeaderGunsmith from "./HeaderGunsmith";
-import HeaderAuditor from "./HeaderAuditor";
 import { useRole } from "@/context/RoleContext";
+
+// Dynamically import all headers with a shared loading configuration
+const LazyHeaderDev = dynamic(() => import("./HeaderDev"), { ssr: false });
+const LazyHeaderAdmin = dynamic(() => import("./HeaderAdmin"), { ssr: false });
+const LazyHeaderSuperAdmin = dynamic(() => import("./HeaderSuperAdmin"), {
+  ssr: false,
+});
+const LazyHeaderPublic = dynamic(() => import("./HeaderPublic"), {
+  ssr: false,
+});
+const LazyHeaderCustomer = dynamic(() => import("./HeaderCustomer"), {
+  ssr: false,
+});
+const LazyHeaderGunsmith = dynamic(() => import("./HeaderGunsmith"), {
+  ssr: false,
+});
+const LazyHeaderAuditor = dynamic(() => import("./HeaderAuditor"), {
+  ssr: false,
+});
+const LazyHeaderUser = dynamic(() => import("./HeaderUser"), { ssr: false });
 
 export default function Header() {
   const { role, loading: isLoading, error } = useRole();
-
-  const LazyHeaderDev = dynamic(
-    () => import("./HeaderDev").then((module) => ({ default: module.default })),
-    { loading: () => <LoadingIndicator /> }
-  );
-
-  const LazyHeaderAdmin = dynamic(
-    () =>
-      import("./HeaderAdmin").then((module) => ({ default: module.default })),
-    { loading: () => <LoadingIndicator /> }
-  );
-
-  const LazyHeaderCeo = dynamic(
-    () =>
-      import("./HeaderAdmin").then((module) => ({ default: module.default })),
-    { loading: () => <LoadingIndicator /> }
-  );
-
-  const LazyHeaderSuperAdmin = dynamic(
-    () =>
-      import("./HeaderSuperAdmin").then((module) => ({
-        default: module.default,
-      })),
-    { loading: () => <LoadingIndicator /> }
-  );
-
-  const LazyHeaderPublic = dynamic(
-    () =>
-      import("./HeaderPublic").then((module) => ({ default: module.default })),
-    { loading: () => <LoadingIndicator /> }
-  );
-
-  const LazyHeaderCustomer = dynamic(
-    () =>
-      import("./HeaderCustomer").then((module) => ({
-        default: module.default,
-      })),
-    { loading: () => <LoadingIndicator /> }
-  );
-
-  const LazyHeaderGunsmith = dynamic(
-    () =>
-      import("./HeaderGunsmith").then((module) => ({
-        default: module.default,
-      })),
-    { loading: () => <LoadingIndicator /> }
-  );
-
-  const LazyHeaderAuditor = dynamic(
-    () =>
-      import("./HeaderAuditor").then((module) => ({ default: module.default })),
-    { loading: () => <LoadingIndicator /> }
-  );
-
-  const LazyHeaderUser = dynamic(
-    () =>
-      import("./HeaderUser").then((module) => ({ default: module.default })),
-    { loading: () => <LoadingIndicator /> }
-  );
 
   if (isLoading) {
     return <LoadingIndicator />;
@@ -92,7 +45,6 @@ export default function Header() {
     case "dev":
       return <LazyHeaderDev />;
     case "admin":
-      return <LazyHeaderAdmin />;
     case "ceo":
       return <LazyHeaderAdmin />;
     case "customer":
