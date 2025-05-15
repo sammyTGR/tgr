d3b50af72bb1199c5dd47dcf257b0c1f12490fe0
Sam Lee <kozyguns@gmail.com>
2025-05-15 08:16:59 -0700

    fixing pnpm conversion errors

--- next.config.js
+++ next.config.js
@@ -4,10 +4,7 @@ const nextConfig = {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
-  },
-  experimental: {
-    ppr: true,
-  },
+  }
 }
 
 module.exports = nextConfig 
