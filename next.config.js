3e419ddf122387d9a27378ac12e1dee323ffa2c2
Sam Lee <kozyguns@gmail.com>
2025-06-18 08:34:26 -0700

    added typescript ignore build errors to next config

--- next.config.js
+++ next.config.js
@@ -4,7 +4,10 @@ const nextConfig = {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
-  }
-}
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+};
 
-module.exports = nextConfig 
+module.exports = nextConfig;
